7b34444cf4a040e69274f045556103c943fe9b1b
Sean Kelly <spkellydev@gmail.com>
2018-07-15 21:54:58 -0400

    Single application transform via constructor

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -6,10 +6,19 @@ import 'dart:async' show Future;
 class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
+  final _items = BehaviorSubject<int>();
+
+  // Single application transformer
+  Observable<Map<int, Future<ItemModel>>> items;
+  StoriesBloc() {
+    items = _items.stream.transform(_itemsTransformer());
+  }
 
   // Getters to Streams
   //// outgoing data to be consumed by widgets
   Observable<List<int>> get topIds => _topIds.stream;
+  //// to Sinks
+  Function(int) get fetchItem => _items.sink.add;
 
   void fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
@@ -26,5 +35,6 @@ class StoriesBloc {
 
   void dispose() {
     _topIds.close();
+    _items.close();
   }
 }
