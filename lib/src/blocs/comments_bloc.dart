abaac559f69e4b24363a231188b72009820cb284
Sean Kelly <spkellydev@gmail.com>
2018-07-19 18:49:19 -0400

    initial implementation of commentsbloc

---
+++ lib/src/blocs/comments_bloc.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
+
+class CommentsBloc {
+  final _repository = Repository();
+  // create sinks
+  final _commentsFetcher = PublishSubject<int>();
+  final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
+
+  // Getters
+  //// Streams
+  Observable<Map<int, Future<ItemModel>>> get itemWithComments =>
+      _commentsOutput.stream;
+
+  //// Sinks
+  Function(int) get fetchItemWithComments => _commentsFetcher.sink.add;
+
+  CommentsBloc() {
+    _commentsFetcher.stream
+        .transform((_commentsTransformer()).pipe(_commentsOutput));
+  }
+
+  _commentsTransformer() {
+    return ScanStreamTransformer<int, Map<int, Future<ItemModel>>>(
+        (cache, int id, int _) {
+      cache[id] = _repository.fetchItem(id);
+      cache[id].then((ItemModel item) {
+        // pass id back into sink to recursively fetch comments
+        item.kids.forEach((kidId) => fetchItemWithComments(kidId));
+      });
+      return cache;
+    }, <int, Future<ItemModel>>{});
+  }
+
+  // close the sink
+  dispose() {
+    _commentsFetcher.close();
+    _commentsOutput.close();
+  }
+}
